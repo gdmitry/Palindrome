a5fda93e4055a228354d10a56578c6d0c11add5b
Dmitry Goncharenko <dmitry.goncharenko24@gmail.com>
2015-10-25 01:29:43 +0300

    Jquery added

--- dev/version 2/palindrome.js
+++ dev/version 2/palindrome.js
@@ -1,29 +1,30 @@
 'use strict'
 
-function testPalyndrom(testString) {	
-		var i, j;
-		for (i = 0; i < testString.length; i++) {
-			for (j = i+2; j < testString.length; j++) {
-				checkIfPalyndrom(testString.substring(i, j+1));
-			}
-		}			
-	};
-	
-	function checkIfPalyndrom(str) {	
-		var i = 0,
-			j = str.length-1;
-		
-		while ((str[i] === str[j]) && (i !== j)) {
-				i++;
-				j--;
-		}			
-		if (i === j) {
-			console.log("+++", str);
-			return str;
+function testPalyndrom(testString) {
+	var i, j;
+	for (i = 0; i < testString.length; i++) {
+		for (j = i + 2; j < testString.length; j++) {
+			checkIfPalyndrom(testString.substring(i, j + 1));
 		}
-		return false;
 	}
+};
 
-   document.querySelectorAll('.sendButton')[0].addEventListener('click', function(e){
-	   testPalyndrom(document.querySelectorAll('.input-field')[0].value);
-   });
+function checkIfPalyndrom(str) {
+	var i = 0,
+		j = str.length - 1;
+
+	while ((str[i] === str[j]) && (i !== j)) {
+		i++;
+		j--;
+	}
+	if (i === j) {
+		console.log("+++", str);
+		return str;
+	}
+	return false;
+}
+
+$(".sendButton").click(function () {
+	testPalyndrom($("input.input-field").val());
+	return false;
+});
