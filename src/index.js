d30b3063d81324ddfc13a607b860e9231887f507
Dmitry Goncharenko <dmitry.goncharenko24@gmail.com>
2015-10-27 03:37:59 +0200

    checkbox problem

--- src/index.js
+++ src/index.js
@@ -3,14 +3,31 @@
 var palindrome = require('./Palindrome.js'),
 	$ = require('../node_modules/jquery/dist/jquery.js');
 
-
 $(".sendButton").click(function () {
-	var testString = $(".input-field").val();
-	palindrome.testPalyndrom(testString);
+	var testString = $(".test-string").val();
+	var ignoreCase = $('.case').prop('checked')
+
+	console.log(ignoreCase);
+	displayResults(palindrome.testPalyndrom(testString, ignoreCase));
 	return false;
 });
 
 $(".resetButton").click(function () {
-	$(".input-field").val('ggfg');
+	$(".test-string").val('');
+	$(".fix").removeClass('result');
 	return false;
 });
+
+function displayResults(results) {
+	var list = $('.result-list');
+
+	results.forEach(function (item) {
+		list.append('<li>' + item + '</li>');
+	});
+
+	if (results.length) {
+		$(".fix").addClass('result');
+	} else {
+		$(".fix").removeClass('result');
+	}
+}
